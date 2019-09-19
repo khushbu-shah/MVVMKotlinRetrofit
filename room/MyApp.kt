09b113ea83822c18391c94fde0b4dc7ce8e790ab
khushbu-shah <khushbu90.shah@gmail.com>
2019-09-19 12:40:53 +0530

    Add files via upload

--- room/MyApp.kt
+++ room/MyApp.kt
@@ -1,1 +1,19 @@
+package com.mvvm.demo.app
 
+import android.app.Application
+import com.mvvm.demo.service.room.model.UserDatabase
+
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        /*MyApp.database= Room.databaseBuilder(this, UserDatabase::class.java, "users.db").allowMainThreadQueries()
+                .build()*/
+
+        UserDatabase.getInstance(this)
+    }
+    companion object {
+         lateinit var database:  UserDatabase
+    }
+}
